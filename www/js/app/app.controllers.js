a420f83e49def63d22e884746942caa3093428ce
Gustavo Andrade de Vilhena <gustavo.vilhena@outlook.com>
2017-05-07 21:19:47 -0300

    Nova mensagem
    Nova mensagem

--- www/js/app/app.controllers.js
+++ www/js/app/app.controllers.js
@@ -268,9 +268,29 @@ angular.module('your_app_name.app.controllers', [])
     
 })
 
-.controller('NovaMsgCtrl', function(){
+
+.controller('NovaMsgCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+  
+  // Load the data
+  $http.get('http://www.corsproxy.com/loripsum.net/api/plaintext').then(function (res) {
+		$scope.textocartao = res.data;
+    $timeout(expand, 0);
+  });
+  
+  $scope.autoExpand = function(e) {
+        var element = typeof e === 'object' ? e.target : document.getElementById(e);
+    		var scrollHeight = element.scrollHeight -60; // replace 60 by the sum of padding-top and padding-bottom
+        element.style.height =  scrollHeight + "px";    
+    };
+  
+  function expand() {
+    $scope.autoExpand('TextArea');
+  }
     
-})
+
+    
+    
+}]);
 
 
-;
+
